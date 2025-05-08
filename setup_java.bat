6d84dd371836d9fd303535ea9c21ac1eeecc4f8f
mohammednada-mi <mohammadmnada@gmail.com>
2025-05-08 17:35:26 +0300

    تحديث إعدادات Codemagic وملفات التوقيع

---
+++ setup_java.bat
@@ -0,0 +1,6 @@
+@echo off
+setx JAVA_HOME "C:\Users\LAPTOP\AppData\Local\Programs\Eclipse Adoptium\jdk-17.0.15.6-hotspot"
+setx PATH "%PATH%;%JAVA_HOME%\bin"
+echo Java environment variables have been set.
+echo Please restart your terminal for changes to take effect.
+pause 
